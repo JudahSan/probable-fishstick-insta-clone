f88a6418f9ecc0fa3bdbccf1070810bf704f05e8
JudahSan <baraka.judah@yahoo.com>
2023-03-01 19:36:05 +0300

    login page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,6 +13,48 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      html: {
+        height: '100%',
+      },
+      body: {
+        backgroundColor: '#fafafa',
+        height: '100%',
+      },
+      '.home-sections': {
+        height: '100%',
+        width: '80%',
+      },
+      '.images-section, .login-section': {
+        height: '95%',
+        width: '45%',
+        backgroundPositionX: 'right',
+      },
+      '.images-section': {
+        backgroundImage: 'url(home-phone-bg.jpg)',
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        maxWidth: '100%',
+        maxHeight: '100%',
+      },
+      '.card-img-top': {
+        maxWidth: '50',
+        alignSelf: 'center',
+      },
+      '.line-text': {
+        padding: '0.5rem',
+        fontSize: 'small',
+        margin: '0',
+      },
+      '.line': {
+        flex: '1',
+        height: '1px',
+        backgroundColor: 'rgb(139, 137, 137)',
+      },
+      '.facebook': {
+        maxWidth: '1rem',
+        height: 'auto',
+        marginRight: '.5rem',
+      },
     },
   },
   plugins: [
